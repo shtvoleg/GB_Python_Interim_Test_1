c972ed9864d25c4951f17188e878794909f52661
shtv_oleg <ov09122014@mail.ru>
2023-09-25 21:09:51 +0400

    GB_Python_Interim_Test_1

--- Shitov_OV_Interim_Test_1.py
+++ Shitov_OV_Interim_Test_1.py
@@ -15,7 +15,7 @@
 import json
 from datetime import datetime
 
-#Функция для загрузки заметок из файла
+#Функция для загрузки заметок из файла 
 def load_notes():
     try:
         with open("Notes.json", "r") as file:
